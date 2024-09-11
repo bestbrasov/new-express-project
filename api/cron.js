7f7d49024ffc381e6d89227d1630be4716dc680c
Razvan Muntean <munteanr20@gmail.com>
2024-09-11 23:07:01 +0300

    sper sa mearga

--- api/cron.js
+++ api/cron.js
@@ -1,14 +1,11 @@
-export default function handler(req, res) {
-    // Call the function to log the time
-    logCurrentTime();
-    
-    // Respond to the HTTP request
-    res.status(200).end('Cron job ran at ' + new Date().toISOString());
+import { fetchCourses } from '../fetch_courses.js';
+
+export default async function handler(req, res) {
+  try {
+    await fetchCourses();
+    res.status(200).send('Courses fetched and inserted successfully.');
+  } catch (error) {
+    console.error('Error in /fetch-courses route:', error);
+    res.status(500).send('Failed to fetch and insert courses');
   }
-  
-  // Function to log the current time to the console
-  function logCurrentTime() {
-    const now = new Date();
-    console.log(`Cron job executed at: ${now.toISOString()}`);
-  }
-  
+}
