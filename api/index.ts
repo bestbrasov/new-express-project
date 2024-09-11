473c09475f331ea9d08126a435cbf040b80285ff
Razvan Muntean <munteanr20@gmail.com>
2024-09-11 21:12:43 +0300

    update

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
-import { sql } from '@vercel/postgres';
+// import { sql } from '@vercel/postgres';
 
 const app = express();
 const port = 3004;
